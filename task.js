db41594989b51d087219585fa24a666f8e74ffc9
Vlad <vladbychek3@gmail.com>
2022-10-18 06:57:37 +0300

    created rep

---
+++ task.js
@@ -0,0 +1,18 @@
+// getMoney('https://www.nbrb.by/api/exrates/rates')
+
+// async function getMoney(url){
+//     let MainUrl = await fetch(url)
+//     let content = await MainUrl.json()
+//     console.log(content)
+//     for(key of content){
+//         let name = key.Cur_Name
+//         let option = document.createElement('option')
+//         let select = document.getElementById('select_value')
+//         option.append(name)
+//         select.append(option)
+//     }
+// }
+
+
+
+
