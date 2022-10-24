fcabb019e0415fcb877df121c98ed97d86a8f5ce
Vlad <vladbychek3@gmail.com>
2022-10-24 13:32:27 +0300

    working

--- task.js
+++ task.js
@@ -1,18 +1,46 @@
-// getMoney('https://www.nbrb.by/api/exrates/rates')
-
-// async function getMoney(url){
-//     let MainUrl = await fetch(url)
-//     let content = await MainUrl.json()
-//     console.log(content)
-//     for(key of content){
-//         let name = key.Cur_Name
-//         let option = document.createElement('option')
-//         let select = document.getElementById('select_value')
-//         option.append(name)
-//         select.append(option)
-//     }
-// }
+getMoney('https://www.nbrb.by/api/exrates/currencies')
 
 
 
 
+
+async function getMoney(url){
+    let MainUrl = await fetch(url)
+    let content = await MainUrl.json()
+
+   let q = []
+
+   for(key of content){
+      let a = new Date(key.Cur_DateStart).getTime()
+      let b = new Date(key.Cur_DateEnd).getTime()
+      q.push({a,b})
+   }
+   // console.log(q)
+
+
+
+
+   // console.dir(newArr); 
+
+
+   //  for(key of content){
+   //    let name = key
+   //    let option = document.createElement('option')
+   //    let select = document.getElementById('select_value')
+   //    option.append(name)
+   //    select.append(option)
+   //  }
+}
+
+let curID = 190
+let startDate = '2016-6-1'
+let endDate = '2016-6-30'
+
+async function getMore(url){
+
+   let MainUrl = await fetch(url)
+   let content = await MainUrl.json()
+   console.log(content)
+}
+
+getMore('https://www.nbrb.by/API/ExRates/Rates/Dynamics/'+ curID +'?startDate='+ startDate +'&endDate='+ endDate +'')
